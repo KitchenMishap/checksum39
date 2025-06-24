6fa377555e0bfe4e906d1721363320098233a877
Kitchen Mishap <hello@kitchenmishap.art>
2025-06-24 14:30:47 +0100

    An on screen keyboard rendered in html. Buttons write into text input

---
+++ app.js
@@ -0,0 +1,37 @@
+function OnLoad() {
+    RenderKbForWord();
+}
+
+function RenderKbForWord() {
+    var topRow="QWERTYUIOP";
+    var midRow = "ASDFGHJKL";
+    var botRow = "ZXCVBNM";
+    var top = DrawKbRow(topRow);
+    var mid = DrawKbRow(midRow);
+    var bot = DrawKbRow(botRow);
+    var el = document.getElementById("kbForWord");
+    el.setHTMLUnsafe(top + "<br>" + DrawHalfKeyGap() + mid + "<br>" + DrawKeyGap() + bot);
+}
+
+function DrawKbRow(keys) {
+    var result="";
+    for (var i = 0; i < keys.length; i++) {
+        result += DrawKeyButton(keys[i]) + "\n";
+    }
+    return result;
+}
+
+function DrawKeyButton(ch) {
+    return "<button onclick='KbPressForWord(\"" + ch + "\")'>" + ch + "</button>";
+}
+
+function DrawHalfKeyGap() {
+    return "&nbsp;&nbsp;&nbsp;";
+}
+
+function DrawKeyGap() {return DrawHalfKeyGap() + DrawHalfKeyGap();}
+
+function KbPressForWord(ch) {
+    var el = document.getElementById("wordstart");
+    el.setAttribute("value", el.getAttribute("value") +ch);
+}
